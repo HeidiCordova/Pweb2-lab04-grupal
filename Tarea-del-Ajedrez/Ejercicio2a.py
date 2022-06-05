7a8484f8fdb4acd38d36a0fd597ae7613718e9a9
hcordova21 <hcordovas@unsa.edu.pe>
2022-06-04 22:02:16 -0500

    crear repositorio

---
+++ Tarea-del-Ajedrez/Ejercicio2a.py
@@ -0,0 +1,6 @@
+from interpreter import draw
+from chessPictures import *
+
+"draw(knight.verticalMirror() and knight.horizontalMirror())"
+"X=[[knight.verticalMirror,queen]]"
+draw(bishop.horizontalMirror)
