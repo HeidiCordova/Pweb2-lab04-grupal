b3fc9e22be425397927af841df9bd93296e64c57
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-06-04 22:03:15 -0500

    Update Ejercicio2a.py

--- Tarea-del-Ajedrez/Ejercicio2a.py
+++ Tarea-del-Ajedrez/Ejercicio2a.py
@@ -1,6 +1,3 @@
 from interpreter import draw
 from chessPictures import *
 
-"draw(knight.verticalMirror() and knight.horizontalMirror())"
-"X=[[knight.verticalMirror,queen]]"
-draw(bishop.horizontalMirror)
