039d52032bc6c6cf42a1ff7e04ce36a47c769868
Claudia Contreras Mamani <ccontrerasma@unsa.edu.pe>
2022-06-05 22:58:27 -0500

    Ejercicio 2a

--- src/Ejercicio2a.py
+++ src/Ejercicio2a.py
@@ -1,3 +1,7 @@
 from interpreter import draw
 from chessPictures import *
-
+primero = knight.negative()
+lin1 = knight.join(primero)
+lin2 = primero.join(knight)
+tercero = lin1.up(lin2)
+draw(tercero)
