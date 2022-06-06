d4dd8868ef1cbb620dd6f8a611d542b28d4ae9af
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-06-05 23:01:57 -0500

    Update Ejercicio2f.py

--- src/Ejercicio2f.py
+++ src/Ejercicio2f.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+negativo= square.negative()
+
+lin1= negativo.join(square)
+lin2=square.join(negativo)
+union= lin1.up(lin2)
+union2 =union.horizontalRepeat(3)
+draw(union2.verticalRepeat(2))
