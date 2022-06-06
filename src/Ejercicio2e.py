4d43e4daf751035189334d2b5053d87cac0d8932
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2022-06-05 23:03:00 -0500

    ejercicio 2e

--- src/Ejercicio2e.py
+++ src/Ejercicio2e.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+#crear un patron
+negativo= square.negative()
+unir= negativo.join(square)
+#repetir el patron
+draw(unir.horizontalRepeat(3))
